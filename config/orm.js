6d0b3ef614337ab473603a0a30580ba676fae0ca
Brandon Bayer <brandonabayer@gmail.com>
2020-03-24 17:33:07 -0500

    [config]working server response

---
+++ config/orm.js
@@ -0,0 +1,38 @@
+const connection = require("./connection.js");
+
+const orm = {
+    all: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create: function (table, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+        queryString += " (name) VALUES (?)";
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    update: function (table, condition, cb) {
+        var queryString = "UPDATE " + table;
+        queryString += " SET ";
+        queryString += "devoured = 1";
+        queryString += " WHERE ";
+        queryString += condition;
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,31 @@
+const burger = require("../models/burger");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", function (req, res) {
+    res.redirect("/api/all");
+});
+
+router.get("/api/all", function (req, res) {
+    burger.all(function (data) {
+        res.render("index", { burger: data });
+    });
+});
+
+router.post("/api/createburger", function (req, res) {
+    console.log(req.body);
+    burger.create(req.body.burger, function (data) {
+        console.log(data);
+    });
+    //return res.redirect("/");
+});
+
+router.put("/api/updateburger/:id", function (req, res) {
+    burger.update("id=" + req.params.id, function (data) {
+        console.log(data);
+    });
+    console.log(res);
+    return res
+});
+
+module.exports = router;
